b84acb5ad22bd5ab2357d4b7ea2af784bd442250
habib <durodolahabib043@gmail.com>
2021-01-17 15:36:50 -0500

    update TC

--- LobLawsEcommerceTests/LobLawsEcommerceTests.swift
+++ LobLawsEcommerceTests/LobLawsEcommerceTests.swift
@@ -10,13 +10,6 @@ import XCTest
 
 class LobLawsEcommerceTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
     func testDisplayName() {
         let homeMVVM = HomeModelView()
@@ -32,13 +25,6 @@ class LobLawsEcommerceTests: XCTestCase {
         XCTAssertNotEqual(homeMVVM.displayPrice(price: "$2.4"), strikeOutText)
 
 
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
